8334d3293d60f6eaff8817d6a38bdc0c8c2969e6
danielMartins <daniel.silvabmartins@gmail.com>
2025-07-02 19:33:13 -0300

    feature: criação da pagina de contato

--- prova-versionamento-main/src/app/page.jsx
+++ prova-versionamento-main/src/app/page.jsx
@@ -4,8 +4,6 @@ export default function Home() {
   return (
     <><div className="w-full h-full bg-gray-200">
       <h1 className=" text-slate-800">Seja Bem-vindo à página de Contato</h1>
-      <p className="font-bold">Daniel da Silva B. Martins</p>
-      <p>Cataguases, 2 de julho de 2025</p>
       </div>
     </>
   )
