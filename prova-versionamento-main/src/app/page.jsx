c3e5b8b97059893240b68c2366d62a688af8d13e
danielMartins <daniel.silvabmartins@gmail.com>
2025-07-02 19:25:23 -0300

    feature: criação da pagina inicial

--- prova-versionamento-main/src/app/page.jsx
+++ prova-versionamento-main/src/app/page.jsx
@@ -1,6 +1,12 @@
+import "tailwindcss"
+
 export default function Home() {
   return (
-    <>
+    <><div className="w-full h-full bg-gray-200">
+      <h1 className=" text-gray-800">“Prova Versionamento - RPV Senai Cataguases</h1>
+      <p className="font-bold">Daniel da Silva B. Martins</p>
+      <p>Cataguases, 2 de julho de 2025</p>
+      </div>
     </>
   )
 }
